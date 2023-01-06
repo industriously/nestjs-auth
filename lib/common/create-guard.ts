d04d3813dd888317c5ae157ef5b0e64da27270fc
industriously <dev.rojiwon@gmail.com>
2023-01-06 21:46:05 +0900

    add AuthGuard Injectable Provider

--- lib/common/create-guard.ts
+++ lib/common/create-guard.ts
@@ -1,38 +1,25 @@
-import { GuardVerify, Strategy } from '@INTERFACE/common.interface';
-import {
-  CanActivate,
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { IAuthGuard } from '@INTERFACE/common.interface';
+import { Strategy } from '@INTERFACE/common.interface';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Request, Response } from 'express';
 
-/**
- *
- * @param strategy oauth2 인증관련 세부 정보와 로직을 가지고 있다.
- * @param verify 인증 정보를 검토한다. default로 무조건 허용한다.
- * @returns Injectable Provider implemented CanActivate interface
- * @throws verify 실패시 verify함수의 두번째 인자를 throw한다.
- */
-export const createAuthGuard = (
-  strategy: Strategy,
-  exception: unknown,
-  verify: GuardVerify = () => true,
-) => {
-  @Injectable()
-  class Guard implements CanActivate {
-    async canActivate(context: ExecutionContext): Promise<boolean> {
-      const request: Request = context.switchToHttp().getRequest();
-      const response: Response = context.switchToHttp().getResponse();
-      const path: string = request.route.path;
+@Injectable()
+export class AuthGuard implements IAuthGuard {
+  constructor(private readonly strategy: Strategy) {}
 
-      strategy.isOauthCallback(path)
-        ? await strategy.authorize(request)
-        : response.redirect(strategy.OAUTH2_URI);
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest();
+    const response: Response = context.switchToHttp().getResponse();
+    const path: string = request.route.path;
 
-      return verify(request, exception ?? new UnauthorizedException());
-    }
+    this.strategy.isOauthCallback(path)
+      ? await this.strategy.authorize(request)
+      : response.redirect(this.strategy.OAUTH2_URI);
+
+    return this.validate(request);
   }
 
-  return Guard;
-};
+  validate(request: Request) {
+    return true;
+  }
+}
--- lib/interface/common.interface.ts
+++ lib/interface/common.interface.ts
@@ -1,4 +1,14 @@
+import { CanActivate } from '@nestjs/common';
 import { Request } from 'express';
+
+export interface IAuthGuard extends CanActivate {
+  /**
+   * request를 통해 인증 정보가 유효한지 확인한다.
+   * @param request
+   * @returns false를 반환하면 ForbiddenException를 반환한다.
+   */
+  validate: (request: Request) => boolean;
+}
 
 export interface Strategy {
   readonly OAUTH2_URI: string;
@@ -6,4 +16,4 @@ export interface Strategy {
   readonly authorize: (request: Request) => Promise<void>;
 }
 
-export type GuardVerify = (req: Request, exception: unknown) => true;
+export type GuardVerify = (req: Request) => true;
