ad076721cb69108ff2fb14906ff1f8692d6e5535
industriously <dev.rojiwon@gmail.com>
2023-01-07 20:00:22 +0900

    code 수정

--- lib/interface/common.interface.ts
+++ lib/interface/common.interface.ts
@@ -1,19 +1,11 @@
-import { CanActivate } from '@nestjs/common';
 import { Request } from 'express';
-
-export interface IAuthGuard extends CanActivate {
-  /**
-   * request를 통해 인증 정보가 유효한지 확인한다.
-   * @param request
-   * @returns false를 반환하면 ForbiddenException를 반환한다.
-   */
-  validate: (request: Request) => boolean;
-}
 
 export interface Strategy {
   readonly OAUTH2_URI: string;
-  readonly isOauthCallback: (path: string) => boolean;
+  readonly redirect_uri: string;
+  readonly isOauthCallback: (request: Request) => boolean;
   readonly authorize: (request: Request) => Promise<void>;
+  readonly validate: (request: Request) => boolean;
 }
 
 export type GuardVerify = (req: Request) => true;
