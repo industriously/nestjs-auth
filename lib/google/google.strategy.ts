0e423d9b23563234d2e72e9c943419ccd83b45aa
노지원 <116284195+industriously@users.noreply.github.com>
2023-01-08 15:08:59 +0900

    strategy Injectable provider (#6)
    * 1.0.2
    
    * setting update
    
    * strategy Injectable provider

--- lib/google/google.strategy.ts
+++ lib/google/google.strategy.ts
@@ -1,23 +1,33 @@
 import { Strategy } from '@INTERFACE/common.interface';
 import { GoogleOauth2Options } from '@INTERFACE/google.interface';
+import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
+import { Credentials } from 'google-auth-library';
 import { decode_jwt } from 'lib/util';
 import { get_client, get_credentials, get_oauth2_uri } from './api';
 
-export const GoogleStrategy = (options: GoogleOauth2Options): Strategy => {
-  const client = get_client(options);
-  return {
-    OAUTH2_URI: get_oauth2_uri(client)(options),
-    isOauthCallback(path) {
-      return path === options.redirect_uri;
-    },
-    async authorize(request) {
-      const code = request.query.code;
-      if (typeof code !== 'string') {
-        return;
-      }
-      const { id_token } = await get_credentials(client)(code);
-      (request as any).user = decode_jwt(id_token as string);
+@Injectable()
+export class GoogleStrategy implements Strategy {
+  readonly OAUTH2_URI: string;
+  private readonly getCredentials: (code: string) => Promise<Credentials>;
+
+  constructor(private readonly options: GoogleOauth2Options) {
+    const client = get_client(this.options);
+    this.OAUTH2_URI = get_oauth2_uri(client)(options);
+    this.getCredentials = get_credentials(client);
+  }
+
+  isOauthCallback(path: string) {
+    return path === this.options.redirect_uri;
+  }
+
+  async authorize(request: Request): Promise<void> {
+    const code = request.query.code;
+    if (typeof code !== 'string') {
       return;
-    },
-  };
-};
+    }
+    const { id_token } = await this.getCredentials(code);
+    (request as any).user = decode_jwt(id_token as string);
+    return;
+  }
+}
